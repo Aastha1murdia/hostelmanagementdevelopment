eabcc36f230bae231b9e6cdb552dcc9ee148d3aa
divya99467 <53223091+divya99467@users.noreply.github.com>
2021-06-23 23:22:56 +0530

    Update StripePaymentSuccess.jsx

--- src/components/payment/StripePaymentSuccess.jsx
+++ src/components/payment/StripePaymentSuccess.jsx
@@ -4,8 +4,21 @@ import Navbar from "../navbar/Navbar";
 import Footer from "../footer/Footer";
 import Success from "../../success-pay.svg";
 import "./Payment.css";
+import { jsPDF } from "jspdf";
+
 
 const StripePaymentSuccess = () => {
+  const history=useHistory();
+  
+  const pdfDownload = e => {
+    e.preventDefault()
+    let doc = new jsPDF("landscape", 'pt', 'A4');
+    doc.html(document.getElementById('pdf-view'), {
+      callback: () => {
+        doc.save('test.pdf');
+      }
+    });
+}
   const Random = (props) => {
     var max = 100;
     var randomno = Math.random() * max + 1;
@@ -18,6 +31,7 @@ const StripePaymentSuccess = () => {
     <>
       <div className="success-verified-body">
         <Navbar />
+          <div id="pdf-view" >
         <div className="main-content-success-verify">
           <div className="container-fluid">
             <div className=" pt-5 text-center mx-auto my-auto">
@@ -40,6 +54,11 @@ const StripePaymentSuccess = () => {
                 Kindly report to student section with the reference number for
                 payment confirmation
               </div>
+                <div className="previous" style={{ display: 'flex', justifyContent: 'flex-end',marginRight:"40px" }}>
+                      {/* <div style={{marginLeft:"400px",marginRight:"400px"}}> */}
+                      <button style={{backgroundColor:"blueviolet"}} className="submit btn btn-sm"onClick={pdfDownload}>Download as pdf</button>
+                     </div>
+
               <div className="success-email-button  mx-auto mb-5 col-md-6">
                 <button
                   type="submit"
