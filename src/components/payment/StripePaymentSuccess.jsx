68db9d15bb53edc6b1ca4c4e2645d927ad82ecc6
divya99467 <53223091+divya99467@users.noreply.github.com>
2021-06-24 00:15:07 +0530

    Update StripePaymentSuccess.jsx

--- src/components/payment/StripePaymentSuccess.jsx
+++ src/components/payment/StripePaymentSuccess.jsx
@@ -25,19 +25,22 @@ const StripePaymentSuccess = () => {
     return <div>{randomno}</div>;
   };
 
-  const history = useHistory();
+  
 
   return (
     <>
       <div className="success-verified-body">
         <Navbar />
-          <div id="pdf-view" >
+       
         <div className="main-content-success-verify">
           <div className="container-fluid">
             <div className=" pt-5 text-center mx-auto my-auto">
+            
               <img src={Success} alt="" height="305" width="305" />
+              <div id="pdf-view" style={{textAlign:"center",marginRight:"100px",marginTop:"70px"}} >
               <div className="text-center success-verified-header mt-0">
-                Payment Successful ✌
+                Payment Successful 
+                {/* Payment Successful ✌ */}
               </div>
               <div className="col-md-12 mt-3 para">
                 We appreciate your payment! If having any doubt, please contact
@@ -54,18 +57,19 @@ const StripePaymentSuccess = () => {
                 Kindly report to student section with the reference number for
                 payment confirmation
               </div>
+              </div>
                 <div className="previous" style={{ display: 'flex', justifyContent: 'flex-end',marginRight:"40px" }}>
                       {/* <div style={{marginLeft:"400px",marginRight:"400px"}}> */}
                       <button style={{backgroundColor:"blueviolet"}} className="submit btn btn-sm"onClick={pdfDownload}>Download as pdf</button>
                      </div>
 
-              <div className="success-email-button  mx-auto mb-5 col-md-6">
+              <div className="success-email-button  mx-auto mb-5 col-md-6" >
                 <button
                   type="submit"
                   name="success-verified"
                   id="success-verified"
                   onClick={() => history.push("/")}
-                  className="submit btn btn-lg btn-block"
+                  className="submit btn btn-sm btn-block"
                 >
                   <div className="verified-button-text">
                     <span>Go to Home Page</span>
