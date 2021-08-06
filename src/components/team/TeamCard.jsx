924eb37602149728e78cb54b0fc284920c32fc4e
ravi-basrani <54708793+ravi-basrani@users.noreply.github.com>
2021-08-06 18:27:32 +0530

    Update TeamCard.jsx

--- src/components/team/TeamCard.jsx
+++ src/components/team/TeamCard.jsx
@@ -12,7 +12,7 @@ const TeamCard =(props)=>{
                                             <p><img src={props.image} alt="card image"/></p>
                                             <h4 class="card-title">{props.name}</h4>
                                             <p class="card-text">{props.position}</p>
-                                            <a href="https://www.fiverr.com/share/qb8D02" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
+                                            <a href="https://www.fiverr.com/share/qb8D02" class="btn btn-primary btn-sm">More Info</a>
                                         </div>
                                     </div>
                                 </div>
@@ -20,7 +20,7 @@ const TeamCard =(props)=>{
                                     <div class="card">
                                         <div class="card-body text-center mt-4">
                                             <h4 class="card-title">{props.name}</h4>
-                                            <p class="card-text"> This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button. </p>
+                                            <p class="card-text"> Well aware of their key responsibility areas and what they are supposed to do at the workplace. Every individual needs to have some goals in life.</p>
                                             <ul class="list-inline">
                                                 <li class="list-inline-item">
                                                     <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02">
